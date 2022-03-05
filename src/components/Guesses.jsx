b0df982f63924f623682ea7f69f268a32004f4fa
Benson Tsang <df981339@gmail.com>
2022-03-05 02:14:46 -0500

    remove redundant code for showing alerts

--- src/components/Guesses.jsx
+++ src/components/Guesses.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useWord } from "../context/WordProvider";
 import Alerts from "./Alerts";
@@ -6,22 +5,10 @@ import Tile from "./Tile";
 
 const Guesses = () => {
   const [state, dispatch] = useWord();
-  const [showAlerts, setShowAlerts] = useState(true); // UI state
-  const [currAlertsList, setCurrAlertsList] = useState([]);
-
-  useEffect(() => {
-    setCurrAlertsList(state.alerts);
-
-    if (state.alerts.length !== 0) {
-      setShowAlerts(true);
-    } else {
-      setShowAlerts(false);
-    }
-  }, [state.alerts]);
 
   return (
     <GuessGrid>
-      {showAlerts ? <Alerts alertsList={currAlertsList} /> : null}
+      {state.alerts.length !== 0 ? <Alerts alertsList={state.alerts} /> : null}
       {state.tiles.map(({ id, value, status }) => (
         <Tile key={id} value={value} status={status} />
       ))}
