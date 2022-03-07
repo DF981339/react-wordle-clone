eae9d121e68ed03a3d61060537860cadc4512d8e
Benson Tsang <df981339@gmail.com>
2022-03-06 23:02:56 -0500

    add responsive style for grid tiles

--- src/components/Guesses.jsx
+++ src/components/Guesses.jsx
@@ -7,32 +7,51 @@ const Guesses = () => {
   const [state, dispatch] = useWord();
 
   return (
-    <GuessGrid>
-      {state.alerts.length !== 0 ? <Alerts alertsList={state.alerts} /> : null}
-      {state.tiles.map(({ id, value, status, shake, flip, bounce }, index) => (
-        <Tile
-          key={id}
-          id={id}
-          value={value}
-          status={status}
-          shake={shake}
-          flip={flip}
-          index={index}
-          bounce={bounce}
-        />
-      ))}
-    </GuessGrid>
+    <Container>
+      <GuessGrid>
+        {state.alerts.length !== 0 ? (
+          <Alerts alertsList={state.alerts} />
+        ) : null}
+        {state.tiles.map(
+          ({ id, value, status, shake, flip, bounce }, index) => (
+            <Tile
+              key={id}
+              id={id}
+              value={value}
+              status={status}
+              shake={shake}
+              flip={flip}
+              index={index}
+              bounce={bounce}
+            />
+          )
+        )}
+      </GuessGrid>
+    </Container>
   );
 };
 
 export default Guesses;
 
-const GuessGrid = styled.section`
-  display: grid;
+const Container = styled.div`
+  display: flex;
   justify-content: center;
   align-items: center;
+  padding: 10px;
+
+  @media (max-width: 400px) {
+    padding: 5px;
+  }
+`;
+
+const GuessGrid = styled.section`
+  display: grid;
   grid-template-columns: repeat(5, 65px);
   grid-template-rows: repeat(6, 65px);
   gap: 5px;
-  padding: 10px;
+
+  @media (max-width: 400px) {
+    grid-template-columns: repeat(5, 60px);
+    grid-template-rows: repeat(6, 60px);
+  }
 `;
