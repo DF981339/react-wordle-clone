96e3539d2fd3d360796d7fc0470967e3efce8779
Benson Tsang <df981339@gmail.com>
2022-04-07 04:17:54 -0400

    fix bar style

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import { useTheme } from "../context/ThemeProvider";
+import { useGame } from "../context/GameProvider";
 
 const Bar = ({ guess, freq, width }) => {
   const [darkTheme, setDarkTheme] = useTheme();
+  const [gameState, gameDispatch] = useGame();
 
   return (
-    <BarContainer darkTheme={darkTheme} freq={freq}>
+    <BarContainer
+      darkTheme={darkTheme}
+      winRow={gameState.winRow + 1 === guess}
+      freq={freq}
+    >
       <div className="guess">{guess}</div>
 
       <div className="graph">
@@ -36,8 +42,15 @@ const BarContainer = styled.div`
       height: 100%;
       justify-content: ${({ freq }) => (freq > 0 ? "flex-end" : "center")};
       padding-right: ${({ freq }) => (freq > 0 ? "8px" : 0)};
-      background-color: ${(props) =>
-        props.darkTheme ? "var(--dark-mode-wrong)" : "var(--light-mode-wrong)"};
+      background-color: ${(props) => {
+        if (props.winRow)
+          return props.darkTheme
+            ? "var(--dark-mode-correct)"
+            : "var(--light-mode-correct)";
+        return props.darkTheme
+          ? "var(--dark-mode-wrong)"
+          : "var(--light-mode-wrong)";
+      }};
       height: 100%;
       display: flex;
       box-sizing: content-box;
